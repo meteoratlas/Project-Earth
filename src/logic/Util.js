1594c2ac4fa7c2c9fd2305eee34f7995d27bf5c6
New Pitsini <new.pitsini@gmail.com>
2020-01-22 15:35:05 -0700

    Finished testing Util functions

--- src/logic/Util.js
+++ src/logic/Util.js
@@ -24,7 +24,7 @@ export default class Util {
     }
     // need to test thoroughly
     static pointInTri(coords, px, py) {
-        const [x1, y1, x2, y2, x3, y3] = this.worldToLocal(coords);
+        const [x1, y1, x2, y2, x3, y3] = coords;
         const origArea = Math.abs(
             (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
         );
