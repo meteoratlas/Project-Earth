b82a96cec01bf9902fa90dfbe46c2e7a6ca84c56
Adriana Silva <achapellin@gmail.com>
2020-01-26 12:12:00 -0700

    icons arraged, added select input menu -still to be connected

--- src/components/ui/TestMenu.js
+++ src/components/ui/TestMenu.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import {ReactComponent as Arrow} from "./triangle.svg"
+import {ReactComponent as Arrow} from "./triangle.svg";
+import OptionComp from "./selectOpt.js"
 
 class TestMenu extends Component {
   constructor(props) {
@@ -9,16 +10,19 @@ class TestMenu extends Component {
 
   render() {
     return (
-      <div id="sidebar" className={this.props.hidden ? "hidden" : ""}>
+      // <div id="sidebar" className={this.props.hidden ? "hidden" : ""}>
+      <div id="sidebar" className="sidebar">
         <h3>Translate</h3>
 
         {/* display arrows, changed direction in ccs */}
-      <div className="icon-group">
-        <div><Arrow className="icon" id="idArrowUp" alt="Move Up" onClick={this.props.moveUp}/></div>
-        <br/>
-        <div>
-        <Arrow className="icon" id="idArrowLeft" alt="Move Left" onClick={this.props.moveLeft}/>
-        <Arrow className="icon" id="idArrowRight" alt="Move Right" onClick={this.props.moveRight}/>
+        <div className="icon-group">
+        <Arrow className="icon" id="idArrowUp" alt="Move Up" onClick={this.props.moveUp}/>
+        <div className="icon-grip">
+          <Arrow className="icon" id="idArrowLeft" alt="Move Left" onClick={this.props.moveLeft}/>
+          <select className="custom-select custom-select-lg">
+            <OptionComp />
+          </select>
+          <Arrow className="icon" id="idArrowRight" alt="Move Right" onClick={this.props.moveRight}/>
         </div>
         <Arrow className="icon" id="idArrowDown" alt="Move Down" onClick={this.props.moveDown}/>
       </div>
@@ -53,6 +57,7 @@ class TestMenu extends Component {
           Horizontal
         </button>
       </div>
+      
     );
   }
 }
