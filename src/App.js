d68ead29949f39bd2e4cc6086219e68c38451e70
Adriana Silva <achapellin@gmail.com>
2020-03-03 17:18:52 -0700

    Audio on/off working

--- src/App.js
+++ src/App.js
@@ -4,67 +4,90 @@ import TestMenu from "./components/ui/TestMenu";
 import Game from "./pixi/Game";
 import "./App.css";
 import "./TransTable.css";
+import Sound from "react-sound";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.canvasRef = React.createRef();
-        this.UIRef = React.createRef();
-        this.canvas = this.canvasRef.current;
-        this.app = undefined;
-        this.game = undefined;
-        this.width = 1024;
-        this.height = 576;
-        this.state = {};
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.UIRef = React.createRef();
+    this.canvas = this.canvasRef.current;
+    this.app = undefined;
+    this.game = undefined;
+    this.width = 1024;
+    this.height = 576;
+    this.state = { audioStatus: Sound.status.PLAYING };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.canvas = this.canvasRef.current;
+      this.app = new PIXI.Application({
+        width: this.width,
+        height: this.height,
+        backgroundColor: 0x1099bb,
+        view: this.canvas,
+        resolution: window.devicePixelRatio || 1
+      });
+      this.game = new Game(this.app);
+      this.game.resetTable = this.sendResetTableMsg;
+      this.game.toggleUI = this.toggleUIVisible;
+      this.game.start();
+    }, 5);
+  }
+  move = (x, y) => {
+    this.game.translate(x, y);
+  };
+  rotate = (a, oX, oY) => {
+    this.game.rotate(a, oX, oY);
+  };
+  reflect = (a, b, c) => {
+    this.game.reflect(a, b, c);
+  };
+  resetLevel = () => {
+    this.game.loadLevel(this.game.currentLevel);
+  };
+
+  sendResetTableMsg = () => {
+    this.UIRef.current.clearTable();
+  };
+  toggleUIVisible = () => {
+    this.UIRef.current.toggleVisible();
+  };
+  audioStatus = () => {
+    if (this.state.audioStatus === Sound.status.PLAYING) {
+      this.setState({ audioStatus: Sound.status.PAUSED });
+    } else {
+      this.setState({ audioStatus: Sound.status.PLAYING });
     }
-    componentDidMount() {
-        setTimeout(() => {
-            this.canvas = this.canvasRef.current;
-            this.app = new PIXI.Application({
-                width: this.width,
-                height: this.height,
-                backgroundColor: 0x1099bb,
-                view: this.canvas,
-                resolution: window.devicePixelRatio || 1
-            });
-            this.game = new Game(this.app);
-            this.game.resetTable = this.sendResetTableMsg;
-            this.game.toggleUI = this.toggleUIVisible;
-            this.game.start();
-        }, 5);
-    }
-    move = (x, y) => {
-        this.game.translate(x, y);
-    };
-    rotate = (a, oX, oY) => {
-        this.game.rotate(a, oX, oY);
-    };
-    reflect = (a, b, c) => {
-        this.game.reflect(a, b, c);
-    };
-    sendResetTableMsg = () => {
-        this.UIRef.current.clearTable();
-    };
-    toggleUIVisible = () => {
-        this.UIRef.current.toggleVisible();
-    };
-    render() {
-        return (
-            <div id="app-container">
-                <div id="game-container">
-                    <canvas ref={this.canvasRef} />
-                    <TestMenu
-                        ref={this.UIRef}
-                        hidden={false}
-                        moves={this.move}
-                        rotates={this.rotate}
-                        reflects={this.reflect}
-                        resetTable={this.resetTable}
-                    />
-                </div>
-            </div>
-        );
-    }
+  };
+
+  render() {
+    return (
+      <div id="app-container">
+        <Sound
+          url="https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3"
+          playStatus={this.state.audioStatus}
+          playFromPosition={0 /* in milliseconds */}
+          loop={true}
+          // onLoading={this.handleSongLoading}
+          // onPlaying={this.handleSongPlaying}
+          // onFinishedPlaying={this.handleSongFinishedPlaying}
+        />
+        <div id="game-container">
+          <canvas ref={this.canvasRef} />
+          <TestMenu
+            ref={this.UIRef}
+            hidden={false}
+            moves={this.move}
+            rotates={this.rotate}
+            reflects={this.reflect}
+            resetLevel={this.resetLevel}
+            audio={this.audioStatus}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ui/TestMenu.js
+++ src/components/ui/TestMenu.js
@@ -4,210 +4,217 @@ import OptionComp from "./selectOpt.js";
 import { ReactComponent as RotateRight } from "./redo.svg";
 import { ReactComponent as RotateLeft } from "./undo.svg";
 import { ReactComponent as Reflect } from "./reflect.svg";
+import { ReactComponent as Audio } from "./sound.svg";
 import TransTable from "./TransTable";
 import ReactTooltip from "react-tooltip";
 
 class TestMenu extends Component {
-    constructor(props) {
-        super(props);
-        this.limitMove = 5;
-        this.nextAvailIndex = 0; //Next available index of transTable
-        this.commands = [];
-        this.state = {
-            hidden: true
-        };
+  constructor(props) {
+    super(props);
+    this.limitMove = 5;
+    this.nextAvailIndex = 0; //Next available index of transTable
+    this.commands = [];
+    this.state = {
+      hidden: true
+    };
+  }
+
+  timeout = ms => {
+    console.log("timeout loop");
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
+
+  sendInput = async e => {
+    for (const each of this.commands) {
+      if (each.command === "idArrowUp") this.props.moves(0, each.amount);
+      if (each.command === "idArrowDown") this.props.moves(0, -each.amount);
+      if (each.command === "idArrowLeft") this.props.moves(-each.amount, 0);
+      if (each.command === "idArrowRight") this.props.moves(each.amount, 0);
+      if (each.command === "idRotateRight") this.props.rotates(90, 0, 0);
+      if (each.command === "idRotateLeft") this.props.rotates(-90, 0, 0);
+      if (each.command === "idReflectY") this.props.reflects(1, 0, 0);
+      if (each.command === "idReflectX") this.props.reflects(0, 1, 0);
+      await this.timeout(800);
     }
+  };
 
-    timeout = ms => {
-        console.log("timeout loop");
-        return new Promise(resolve => setTimeout(resolve, ms));
-    };
+  addToTable = e => {
+    let command, detail;
 
-    sendInput = async e => {
-        for (const each of this.commands) {
-            if (each.command === "idArrowUp") this.props.moves(0, each.amount);
-            if (each.command === "idArrowDown")
-                this.props.moves(0, -each.amount);
-            if (each.command === "idArrowLeft")
-                this.props.moves(-each.amount, 0);
-            if (each.command === "idArrowRight")
-                this.props.moves(each.amount, 0);
-            if (each.command === "idRotateRight") this.props.rotates(90, 0, 0);
-            if (each.command === "idRotateLeft") this.props.rotates(-90, 0, 0);
-            if (each.command === "idReflectY") this.props.reflects(1, 0, 0);
-            if (each.command === "idReflectX") this.props.reflects(0, 1, 0);
-            await this.timeout(800);
-        }
-    };
+    if (e.target.id === "") return;
 
-    addToTable = e => {
-        let command, detail;
+    // if table is full
+    if (this.nextAvailIndex === this.limitMove)
+      return alert("Cannot add more moves to the table.");
 
-        if (e.target.id === "") return;
+    // if table still have row available
+    if (this.nextAvailIndex < this.limitMove) {
+      // store the command in array
+      this.commands.push({
+        command: e.target.id,
+        amount: Number(this.unitsMove.value)
+      });
 
-        // if table is full
-        if (this.nextAvailIndex === this.limitMove)
-            return alert("Cannot add more moves to the table.");
+      detail = this.unitsMove.value + " position(s)";
+      if (e.target.id === "idArrowUp") command = "Move Up";
+      if (e.target.id === "idArrowDown") command = "Move Down";
+      if (e.target.id === "idArrowLeft") command = "Move Left";
+      if (e.target.id === "idArrowRight") command = "Move Right";
 
-        // if table still have row available
-        if (this.nextAvailIndex < this.limitMove) {
-            // store the command in array
-            this.commands.push({
-                command: e.target.id,
-                amount: Number(this.unitsMove.value)
-            });
+      if (e.target.id === "idRotateRight") {
+        command = "Rotate Right";
+        detail = "90 degrees";
+      }
 
-            detail = this.unitsMove.value + " position(s)";
-            if (e.target.id === "idArrowUp") command = "Move Up";
-            if (e.target.id === "idArrowDown") command = "Move Down";
-            if (e.target.id === "idArrowLeft") command = "Move Left";
-            if (e.target.id === "idArrowRight") command = "Move Right";
+      if (e.target.id === "idRotateLeft") {
+        command = "Rotate Left";
+        detail = "90 degrees";
+      }
 
-            if (e.target.id === "idRotateRight") {
-                command = "Rotate Right";
-                detail = "90 degrees";
-            }
+      if (e.target.id === "idReflectY") {
+        command = "Reflection";
+        detail = "Y Axis";
+      }
 
-            if (e.target.id === "idRotateLeft") {
-                command = "Rotate Left";
-                detail = "90 degrees";
-            }
+      if (e.target.id === "idReflectX") {
+        command = "Reflection";
+        detail = "X Axis";
+      }
 
-            if (e.target.id === "idReflectY") {
-                command = "Reflection";
-                detail = "Y Axis";
-            }
+      document.getElementsByClassName("move")[
+        this.nextAvailIndex
+      ].textContent = command;
+      document.getElementsByClassName("detail")[
+        this.nextAvailIndex
+      ].textContent = detail;
 
-            if (e.target.id === "idReflectX") {
-                command = "Reflection";
-                detail = "X Axis";
-            }
+      this.nextAvailIndex++;
+    }
+  };
 
-            document.getElementsByClassName("move")[
-                this.nextAvailIndex
-            ].textContent = command;
-            document.getElementsByClassName("detail")[
-                this.nextAvailIndex
-            ].textContent = detail;
+  toggleVisible = () => {
+    console.log("toggle");
+    this.setState(prev => ({
+      hidden: !prev.hidden
+    }));
+  };
 
-            this.nextAvailIndex++;
-        }
-    };
+  clearTable = () => {
+    for (let i = 0; i < this.limitMove; i++) {
+      document.getElementsByClassName("move")[i].textContent = "";
+      document.getElementsByClassName("detail")[i].textContent = "";
+    }
+    this.props.resetLevel();
+    this.toggleVisible();
+    //reset variable
+    this.commands = [];
+    this.nextAvailIndex = 0;
+  };
 
-    toggleVisible = () => {
-        console.log("toggle");
-        this.setState(prev => ({
-            hidden: !prev.hidden
-        }));
-    };
+  render() {
+    return (
+      <div id="sidebar" className={this.state.hidden ? "hidden" : "show"}>
+        {/* display arrows, changed direction in ccs */}
 
-    clearTable = () => {
-        for (let i = 0; i < this.limitMove; i++) {
-            document.getElementsByClassName("move")[i].textContent = "";
-            document.getElementsByClassName("detail")[i].textContent = "";
-        }
+        <Audio
+          className="icon"
+          id="idSoundOn"
+          alt="Sound On"
+          onClick={this.props.audio}
+        />
 
-        //reset variable
-        this.commands = [];
-        this.nextAvailIndex = 0;
-    };
+        <div className="icon-group">
+          <div className="icon-grid">
+            <ReactTooltip />
+            <RotateRight
+              className="icon"
+              id="idRotateRight"
+              alt="Rotate Right"
+              data-tip="Rotate 90 degrees right"
+              onClick={this.addToTable}
+            />
+            <Arrow
+              className="icon"
+              id="idArrowUp"
+              alt="Move Up"
+              data-tip="Translate X units up"
+              onClick={this.addToTable}
+            />
+            <RotateLeft
+              className="icon"
+              id="idRotateLeft"
+              alt="Rotate Left"
+              data-tip="Rotate 90 degrees left"
+              onClick={this.addToTable}
+            />
+          </div>
+          <div className="icon-grid">
+            <Arrow
+              className="icon"
+              id="idArrowLeft"
+              alt="Move Left"
+              data-tip="Translate X units left"
+              onClick={this.addToTable}
+            />
+            <select
+              className="custom-select custom-select-lg"
+              ref={input => {
+                this.unitsMove = input;
+              }}
+              data-tip="Select units to translate by"
+            >
+              <OptionComp />
+            </select>
+            <Arrow
+              className="icon"
+              id="idArrowRight"
+              alt="Move Right"
+              data-tip="Translate X units right"
+              onClick={this.addToTable}
+            />
+          </div>
+          <div className="icon-grid">
+            <Reflect
+              className="icon"
+              id="idReflectY"
+              alt="Reflect Y"
+              data-tip="Reflect across Y axis"
+              onClick={this.addToTable}
+            />
+            <Arrow
+              className="icon"
+              id="idArrowDown"
+              alt="Move Down"
+              data-tip="Translate X units down"
+              onClick={this.addToTable}
+            />
+            <Reflect
+              className="icon"
+              id="idReflectX"
+              alt="Reflect X"
+              data-tip="Reflect across X axis"
+              onClick={this.addToTable}
+            />
+          </div>
+        </div>
 
-    render() {
-        return (
-            <div id="sidebar" className={this.state.hidden ? "hidden" : "show"}>
-                {/* display arrows, changed direction in ccs */}
-                <div className="icon-group">
-                    <div className="icon-grid">
-                        <ReactTooltip />
-                        <RotateRight
-                            className="icon"
-                            id="idRotateRight"
-                            alt="Rotate Right"
-                            data-tip="Rotate 90 degrees right"
-                            onClick={this.addToTable}
-                        />
-                        <Arrow
-                            className="icon"
-                            id="idArrowUp"
-                            alt="Move Up"
-                            data-tip="Translate X units up"
-                            onClick={this.addToTable}
-                        />
-                        <RotateLeft
-                            className="icon"
-                            id="idRotateLeft"
-                            alt="Rotate Left"
-                            data-tip="Rotate 90 degrees left"
-                            onClick={this.addToTable}
-                        />
-                    </div>
-                    <div className="icon-grid">
-                        <Arrow
-                            className="icon"
-                            id="idArrowLeft"
-                            alt="Move Left"
-                            data-tip="Translate X units left"
-                            onClick={this.addToTable}
-                        />
-                        <select
-                            className="custom-select custom-select-lg"
-                            ref={input => {
-                                this.unitsMove = input;
-                            }}
-                            data-tip="Select units to translate by"
-                        >
-                            <OptionComp />
-                        </select>
-                        <Arrow
-                            className="icon"
-                            id="idArrowRight"
-                            alt="Move Right"
-                            data-tip="Translate X units right"
-                            onClick={this.addToTable}
-                        />
-                    </div>
-                    <div className="icon-grid">
-                        <Reflect
-                            className="icon"
-                            id="idReflectY"
-                            alt="Reflect Y"
-                            data-tip="Reflect across Y axis"
-                            onClick={this.addToTable}
-                        />
-                        <Arrow
-                            className="icon"
-                            id="idArrowDown"
-                            alt="Move Down"
-                            data-tip="Translate X units down"
-                            onClick={this.addToTable}
-                        />
-                        <Reflect
-                            className="icon"
-                            id="idReflectX"
-                            alt="Reflect X"
-                            data-tip="Reflect across X axis"
-                            onClick={this.addToTable}
-                        />
-                    </div>
-                </div>
-
-                {/* Transformation Sheet */}
-                <div className="transTable">
-                    <div className="wrapper">
-                        <div className="button">
-                            <button onClick={this.clearTable}>Reset</button>
-                            <button onClick={this.sendInput}>Play All</button>
-                        </div>
-                        <div className="header">
-                            <div className="middle">Move</div>
-                            <div className="middle">Details</div>
-                        </div>
-                        <TransTable limitMove={this.limitMove} />
-                    </div>
-                </div>
+        {/* Transformation Sheet */}
+        <div className="transTable">
+          <div className="wrapper">
+            <div className="button">
+              <button onClick={this.clearTable}>Reset</button>
+              <button onClick={this.sendInput}>Play All</button>
             </div>
-        );
-    }
+            <div className="header">
+              <div className="middle">Move</div>
+              <div className="middle">Details</div>
+            </div>
+            <TransTable limitMove={this.limitMove} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TestMenu;
