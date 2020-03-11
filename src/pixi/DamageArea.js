2b526d66b7fbbc2f93806fb355c4c13f37ea0753
Kelly H <kellyaccnts@gmail.com>
2020-03-10 23:09:48 -0600

    clean up css, texture for damagearea

--- src/pixi/DamageArea.js
+++ src/pixi/DamageArea.js
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js-legacy";
 // import gsap from "gsap";
 import Util from "../logic/Util";
+import Lava from "../resources/lava-256.png";
 
 export default class DamageArea extends PIXI.Graphics {
     constructor(coords, boardSize, cellSize) {
@@ -17,7 +18,9 @@ export default class DamageArea extends PIXI.Graphics {
         let c = Util.localToWorld(this.coords);
         const size = this.cellSize;
         this.clear();
-        this.beginFill(0xff0000);
+        //this.beginFill(0xff0000);
+        const lavaTexture = PIXI.Texture.from(Lava);
+        this.beginTextureFill({ texture: lavaTexture });
         this.lineStyle(2);
 
         this.moveTo(c[0] * size, c[1] * size);
